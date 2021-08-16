5cc8160f0d983b4f6b970d1ff4c8e43fd4f3ca8d
imad <abbadimad0123@gmail.com>
2021-08-16 15:46:10 +0100

    v2.0

--- includes/template/navbar.php
+++ includes/template/navbar.php
@@ -1,7 +1,7 @@
 <!-- Navbar Template * -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="index.php">TRUC<sapn style="color: #5352ed;">TRY</sapn></a>
+    <a class="navbar-brand" href="index.php">TRUC<sapn style="color: var(--btn);">TRY</sapn></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
